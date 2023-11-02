fb11b2a0373de56a06486e6c2144401b6a0dfc7f
DoveTiger1 <cpl.purityloveday@gmail.com>
2023-11-02 18:27:11 +0100

    correcting blog.js

--- blog.js
+++ blog.js
@@ -2,6 +2,7 @@ const links = document.querySelectorAll('.links a');
 
 links.forEach((link) => {
   link.addEventListener('click', () => {
+    document.location.pathname = '/';
     document.location.href = '#' + link.textContent;
   });
 });
