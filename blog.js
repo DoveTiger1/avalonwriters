5d324ff05edee36353901870f1c924fa005dd6bd
DoveTiger1 <cpl.purityloveday@gmail.com>
2023-11-03 02:52:45 +0100

    set up css

--- blog.js
+++ blog.js
@@ -1,10 +1,26 @@
-const links = document.querySelectorAll('.links a');
+const listLinks = document.querySelectorAll('.links a');
 
-links.forEach((link) => {
+listLinks.forEach((link) => {
   link.addEventListener('click', () => {
     document.location.pathname = '/';
     if (link.textContent === 'blog') {
       document.location.href = '/blog.html';
-    } 
+    }
   });
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+  // linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 });
